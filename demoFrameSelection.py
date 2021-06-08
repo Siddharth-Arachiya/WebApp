a896a13362feb740849f96ec7327f1e977aee0a3
rohangrge <rohangrge@gmail.com>
2021-06-08 23:34:24 +0530

    bug fix

--- demoFrameSelection.py
+++ demoFrameSelection.py
@@ -95,6 +95,7 @@ def image(arr):
         # im_bytes = imdict[arr[2]]  #please add some check for if the image does not exist
         r.setex(arr[2], timedelta(minutes=5),
                 value=pickle.dumps(imdict[arr[2]]))
+        im_bytes = pickle.loads(r.get(arr[2]))
         im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
         frame = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
         #frame = gimage.copy()
